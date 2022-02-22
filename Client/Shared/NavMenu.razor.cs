8dfbfffc4e004e3a51bbbd767acbd52fc1037a2c
Sam Hsu <sam.hsu@shl-group.com>
2022-02-22 16:38:48 +0800

    implement component - NavMenu

---
+++ Client/Shared/NavMenu.razor.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Routing;
+
+namespace HandsOnWithBlazor.Client.Shared
+{
+    public partial class NavMenu : ComponentBase
+    {
+        [Inject]
+        private NavigationManager NavigationManager { get; set; }
+
+        private NavLinkMatch _matchMode = NavLinkMatch.All;
+
+        protected override void OnInitialized() => NavigationManager.LocationChanged += (s, e) => StateHasChanged();
+
+
+        private bool IsActive(string href, NavLinkMatch navLinkMatch = NavLinkMatch.Prefix)
+        {
+            var relativePath = NavigationManager.ToBaseRelativePath(NavigationManager.Uri).ToLower();
+            return navLinkMatch == NavLinkMatch.All ? relativePath == href.ToLower() : relativePath.StartsWith(href.ToLower());
+        }
+
+        private string GetActive(string href, NavLinkMatch navLinkMatch = NavLinkMatch.Prefix) => IsActive(href, navLinkMatch) ? "active" : "";
+    }
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,41 @@ module.exports = {
   mode: "jit",
   content: ["./**/*.html", "./**/*.razor"],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        'px-50': '50px',
+        'px-300': '300px',
+        // 'full-2x':'200%'
+      },
+      minWidth: {
+        'px-60':'60px'
+      },
+      height: {
+        'px-50':'50px',
+        'px-60':'60px'
+      },
+      lineHeight: {
+        'px-60':'60px',
+        'px-70':'70px'
+      },
+      borderWidth: {
+        '10' : '10px'
+      },
+      transitionDuration: {
+        '0.5' : '0.5s'
+      },
+      borderRadius: {
+        'px-30': '30px',
+        'p-1/2': '50%'
+      },
+      inset: {
+        '-px-50': '-50px'
+      },
+      boxShadow: {
+        'tr-curve': '35px 35px 0 10px #fff',
+        'br-curve': '35px -35px 0 10px #fff'
+      }      
+    },
   },
   plugins: [],
 };
